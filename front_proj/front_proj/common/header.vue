09cbc727938162614c1d2452785c0c6f473078f3
qianyu0415 <3527966027@qq.com>
2024-11-03 22:42:28 +0800

    second commit

---
+++ front_proj/front_proj/common/header.vue
@@ -0,0 +1,41 @@
+<template>
+  <div style="color: white">
+    <!-- <img src="../assets/vue.svg" alt="" width="20" height="40" style="margin: 10px 90px 10px 50px;float: left" /> -->
+    <i class="el-icon-menu collapse" @click="clickCollapse" style="margin-right: 20px;float: left"></i>
+    <div class="misName">学生成绩管理系统</div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    clickCollapse() {
+      console.log('Menu collapsed/expanded');
+    }
+  }
+};
+</script>
+
+<style scoped>
+  /* 旋转的关闭按钮 */
+  .collapse {
+    font-size: 25px;
+    float: right;
+    line-height: 60px;
+    transition: 0.2s;
+    transform: rotate(-180deg);
+    cursor: pointer;
+  }
+
+  .collapse:hover {
+    transition: 0.2s;
+    transform: rotate(180deg);
+  }
+
+  .misName {
+    margin-left: 50px;
+    float: left;
+    font-size: 24px;
+    line-height: 60px;
+  }
+</style>
---
+++ front_proj/front_proj/src/App.vue
@@ -0,0 +1,60 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+import Header from '../common/header.vue'
+
+
+</script>
+
+<template>
+  <el-container>
+      <el-header class="headColor">
+          <Header></Header>
+      </el-header>
+        <el-container class="header">  
+          <!-- <el-aside class="asideWidth">
+            <Aside @collapse="isCollapse" ref="aside"></Aside>
+          </el-aside> -->
+          <el-main style="background-color: #ededed; padding: 0; height: 100%">
+              <RouterView />
+          </el-main>
+        </el-container>
+  </el-container>
+</template>
+<style>
+    html, body, #app, .home, .el-container {
+      margin: 0 !important;
+      padding: 0 !important;
+      height: 100% !important;
+    }
+  </style>
+  <style scoped>
+    .headColor {
+      height: 70px;
+      background-color: #5a8bff;
+      padding: 0;
+      margin: 0;
+    }
+    .asideWidth {
+      /*background-image: linear-gradient(rgba(90,139,255,1), white);*/
+      border-right: 1px solid #e6e6e6;
+      width: 240px;
+      transition: 0.2s;
+      height: 100%
+    }
+    .el-card__body {
+      padding: 0;
+    }
+    .copyright {
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      height: 24px;
+      line-height: 20px;
+      text-align: center;
+      width: 100%;
+      font-size: 12px;
+      color: #666;
+    }
+  </style>
+  
